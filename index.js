c0294c6f2cff745595540f9ae7e5275c5f8f3fec
Merazul Amin Saikut <merazulamin2122@yahoo.com>
2022-10-18 18:03:15 +0600

    first-commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.Port || 5000;
+app.use(cors());
+const categories = require('./data/categories.json');
+
+app.get('/', (req, res) => {
+    res.send('News Api Running');
+})
+app.listen(port, () => {
+    console.log('Dragon News Server Running on port ', port);
+})
+
+
+app.get('/news-categories', (req, res) => {
+    res.send(categories);
+})
